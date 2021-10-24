cd13a9a1885e96e0d1b952273aa752e76a62bce0
sayyedbasit75 <sayyedbasit75@gmail.com>
2021-10-24 13:16:40 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PersonalLoanComponent } from './components/personal-loan/personal-loan.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/verify", pathMatch: "full" },
+  { path: "verify", component: PersonalLoanComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/personal-loan/personal-loan.component.ts
+++ src/app/components/personal-loan/personal-loan.component.ts
@@ -18,10 +18,12 @@ export class PersonalLoanComponent implements OnInit {
   resendOtpErrMsg: boolean = false //Resend OTP Error Message Flag
   hideResendButton: boolean = true // Hide Resend Button Flag
   isRegistration: boolean = true // Form Flag
+  OtpSendSuccessFlag: boolean = true // OTP Checking Flag
 
   resendOtpCount: number = 0 // OTP Count - how many time resend button clicked
   fullName: string = '' // property used to dispay name on Thank you screen
 
+  timer: string = ''
 
   constructor(
     private fb: FormBuilder,
@@ -36,7 +38,7 @@ export class PersonalLoanComponent implements OnInit {
       panNumber: ['', [Validators.required, Validators.pattern("^([a-zA-Z]){5}([0-9]){4}([a-zA-Z]){1}?$")]],
       fullName: ['', [Validators.required, Validators.maxLength(140)]],
       email: ['', [Validators.required, Validators.email, Validators.maxLength(255)]],
-      mobile: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern('[0-9]*')]],
+      mobile: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
       otp: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(4), Validators.pattern('[0-9]*')]],
     })
   }
@@ -45,23 +47,30 @@ export class PersonalLoanComponent implements OnInit {
   get f() { return this.form.controls }
 
 
+  numberOnly(event: any): boolean {
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    return true;
+
+  }
+
   // Send OTP 
   sendOtp() {
-    let mobileNumber = this.form.controls.mobile.value
 
     if (this.form.controls.mobile.valid) {
       // if mobile number is valid then send OTP
-      if (mobileNumber.length == 10) {
-        let data = this.form.value
+      let data = this.form.value
+      if (this.OtpSendSuccessFlag) {
         this.otpService.sendOTP(data).subscribe((res: sendOtpResponse) => {
+          this.OtpSendSuccessFlag = false
           if (res.statusCode == 200) {
             this.isOtpReceived = true // show OTP Fields
             this.otpResendButton = true // Show OTP Resend Btton
             this.updateResendButton() // Enable OTP Button after 3 Minutes
           } else console.log("Some Thing Failed", res)
         })
-      } else {
-        this.isOtpReceived = false // Hide OTP Field
       }
 
     } else this.isOtpReceived = false  // Hide OTP Field
@@ -83,8 +92,30 @@ export class PersonalLoanComponent implements OnInit {
   }
 
 
+  startTimer() {
+    var counter = 0;
+    var min = 3;
+    var sec = 60;
+    let timer = setInterval(() => {
+      sec = --sec;
+      if (sec === 0) {
+        min = --min;
+        sec = 59;
+        counter = ++counter;
+      }
+      if (counter === 3) {
+        sec = 0;
+        min = 0;
+        clearInterval(timer)
+        document.getElementById("timer")!.innerHTML = ''
+      }
+      document.getElementById("timer")!.innerHTML = min + " : " + sec
+    }, 1000)
+  }
+
   //Enable resend flag after 3 minutes
   updateResendButton() {
+    this.startTimer()
     setTimeout(() => {
       this.otpResendButton = false
     }, 180000)
